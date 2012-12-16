5022f815af7ca829bd1e8a575fc3949441cd6b23
Todd Gerspacher <toddgerspacher27@gmail.com>
2012-12-16 13:18:37 -0500

    End of Chapter 2 : Validating the Hibernate Setup

--- src/com/examscam/model/User.java
+++ src/com/examscam/model/User.java
@@ -1,24 +1,46 @@
 package com.examscam.model;
 
-/* The User Class without JPA annotations */
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+import org.hibernate.cfg.AnnotationConfiguration;
+import org.hibernate.tool.hbm2ddl.SchemaExport;
+
+// Title: Hibernate Made Easy
+// Author: Cameron McKenzie
+// Page: 64 Added JPA Annotation to User Pojo
+// Page: 73 Added static main method to User Pojo
+
+@Entity
 public class User {
 
-    private Long id;
+	private Long id;
 
-    public Long getId() {
-        return id;
-    }
+	@Id
+	@GeneratedValue
+	public Long getId() {
+		return id;
+	}
 
-    public void setId(Long id) {
-        this.id = id;
-    }
-    private String password;
+	public void setId(Long id) {
+		this.id = id;
+	}
 
-    public String getPassword() {
-        return password;
-    }
+	private String password;
 
-    public void setPassword(String password) {
-        this.password = password;
-    }
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public static void main(String[] args) {
+		AnnotationConfiguration config = new AnnotationConfiguration();
+		config.addAnnotatedClass(User.class);
+		config.configure();
+		new SchemaExport(config).create(true, true);
+	}
 }
