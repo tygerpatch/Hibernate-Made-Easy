1815fdf001128fac94d270daa26bcf9b38139ad5
Todd Gerspacher <toddgerspacher27@gmail.com>
2012-12-16 19:43:32 -0500

    updated getInitializedConfiguration method

--- src/com/examscam/HibernateUtil.java
+++ src/com/examscam/HibernateUtil.java
@@ -10,6 +10,7 @@ import com.examscam.model.User;
 
 // Page: 126 start of HibernateUtil
 // Page: 126 getInitializedConfiguration method
+// Page: 127 update getInitializedConfiguration method with reference to future classes
 
 public class HibernateUtil {
 
@@ -18,6 +19,18 @@ public class HibernateUtil {
 
         // add all of your JPA annotated classes here!!!
         config.addAnnotatedClass(User.class);
+
+        // Future classes we will be creating.
+        // Keep them commented out for now.
+
+        //config.addAnnotatedClass(Snafu.class);
+        //config.addAnnotatedClass(FooBar.class);
+        //config.addAnnotatedClass(Thing.class);
+        //config.addAnnotatedClass(Team.class);
+        //config.addAnnotatedClass(Player.class);
+        //config.addAnnotatedClass(RightManyCourse.class);
+        //config.addAnnotatedClass(LeftManyStudent.class);
+
         config.configure();
         return config;
     }
