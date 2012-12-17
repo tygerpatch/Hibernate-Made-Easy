303ab07c999c5cd6e8ac2c792b77ed3531e19f2e
Todd Gerspacher <toddgerspacher27@gmail.com>
2012-12-16 19:37:05 -0500

    End of Chapter 4 : Crud Based Operations

--- src/com/examscam/LoadRunner.java
+++ src/com/examscam/LoadRunner.java
@@ -1,5 +1,6 @@
 package com.examscam;
 
+import org.hibernate.ObjectNotFoundException;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.AnnotationConfiguration;
@@ -10,38 +11,47 @@ import com.examscam.model.User;
 // Author: Cameron McKenzie
 
 // Page: 119 start of LoadRunner class
+// Page: 123 complete LoadRunner class code
 
 public class LoadRunner {
 
-    public static void callLoad() {
-        AnnotationConfiguration config = new AnnotationConfiguration();
-        config.addAnnotatedClass(User.class);
+	public static void callLoad() {
+		AnnotationConfiguration config = new AnnotationConfiguration();
+		config.addAnnotatedClass(User.class);
 
-        SessionFactory factory = config.configure().buildSessionFactory();
-        Session session = factory.getCurrentSession();
-        session.beginTransaction();
+		SessionFactory factory = config.configure().buildSessionFactory();
+		Session session = factory.getCurrentSession();
+		session.beginTransaction();
 
-        // load code will go here
+		try {
+			User user = (User) session.load(User.class, new Long(1));
+			System.out.println(user.getPassword());
+		} catch (ObjectNotFoundException e) {
+			e.printStackTrace();
+		}
 
-        session.getTransaction().commit();
-    }
+		session.getTransaction().commit();
+		// System.out.println(user.getPassword()); // fails
+	}
 
-    public static void callGet() {
-        AnnotationConfiguration config = new AnnotationConfiguration();
-        config.addAnnotatedClass(User.class);
+	public static void callGet() {
+		AnnotationConfiguration config = new AnnotationConfiguration();
+		config.addAnnotatedClass(User.class);
 
-        SessionFactory factory = config.configure().buildSessionFactory();
-        Session session = factory.getCurrentSession();
-        session.beginTransaction();
+		SessionFactory factory = config.configure().buildSessionFactory();
+		Session session = factory.getCurrentSession();
+		session.beginTransaction();
 
-        // get code will go here
+        User user = (User) session.get(User.class, new Long(1));
+        System.out.println(user.getPassword());
 
-        session.getTransaction().commit();
-    }
+		session.getTransaction().commit();
+        System.out.println(user.getPassword());  // no problem
+	}
 
-    public static void main(String[] args) {
-        LoadRunner.callLoad();
-        LoadRunner.callGet();
-    }
+	public static void main(String[] args) {
+		LoadRunner.callLoad();
+		LoadRunner.callGet();
+	}
 
 }
